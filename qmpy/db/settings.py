be55af2356602b17f7144ace087c69970d345fa0
Patrick Herring <patrick.herring@tri.global>
2018-03-13 22:28:59 -0700

    change import

--- qmpy/db/settings.py
+++ qmpy/db/settings.py
@@ -1,5 +1,6 @@
 # Django settings for oqmd project.
-import os
+import os.path
+import os.environ
 INSTALL_PATH = os.path.dirname(os.path.abspath(__file__))
 INSTALL_PATH = os.path.split(INSTALL_PATH)[0]
 INSTALL_PATH = os.path.split(INSTALL_PATH)[0]
@@ -18,8 +19,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DATABASE_NAME', ''),
-        'USER': os.environ.get('DATABASE_USER', ''),
+        'NAME': os.environ.get('DATABASE_NAME', 'example_database'),
+        'USER': os.environ.get('DATABASE_USER', 'admin'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
         'HOST': os.environ.get('DATABASE_HOST', ''),
         'PORT': os.environ.get('DATABASE_PORT', ''),
